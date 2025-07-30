e1cd505ee2a742cdbb2ef9986cc4b650bfd610de
Sanjiv Jangid <sanjivjangid@Sanjivs-MacBook-Air.local>
2025-07-30 15:45:14 +0530

    landiing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Coming soon...
+        <div className="flex flex-col gap-1">
+          <h3 className="underline font-bold mb-1">Ask me about</h3>
+          <p>Javascript, Typescript, React.js, Node.js</p>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>

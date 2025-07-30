7c031f8f5db94df62217515ee14968d8aeb38766
Sanjiv Jangid <sanjivjangid@Sanjivs-MacBook-Air.local>
2025-07-30 21:56:12 +0530

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  distDir: "dist",
+  distDir: "out",
   images: {
     unoptimized: true,
   },
